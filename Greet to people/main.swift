0fd3fc3f25184387317b33a1fe461c787b9f548d
Lingyi Zhan <shinycatherine01@gmail.com>
2020-12-07 23:44:26 -0600

    Initial Commit

---
+++ Greet to people/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Greet to people
+//
+//  Created by Lingyi Zhan on 12/7/20.
+//  Copyright © 2020 Lingyi Zhan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
